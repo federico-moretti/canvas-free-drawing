31ba3c3797e8cd5c51729052e6ba1f3337010146
Federico Moretti <dj.ikos90@gmail.com>
2018-07-07 11:36:36 +0200

    changes to builds

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/canvas-free-drawing.js',
+    file: 'umd/canvas-free-drawing.js',
     format: 'umd',
     name: 'CanvasFreeDrawing',
     sourcemap: true,
